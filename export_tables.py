726e696fe8c10e749afaa617509c664693e5182d
George Azzari from Beta <george.azzari@gmail.com>
2018-01-20 13:09:40 -0800

    Updated harmonics function and added export tables module

---
+++ export_tables.py
@@ -0,0 +1,54 @@
+"""
+Author: George Azzari (gazzari@stanford.edu)
+Center on Food Security and the Environment
+Department of Earth System Science
+Stanford University
+"""
+
+import ee
+
+
+def get_point(image, point, scale, depth):
+
+    s = image.reduceRegion(
+        reducer=ee.Reducer.first(),
+        geometry=point.geometry(),
+        scale=scale,
+        bestEffort=False,
+        maxPixels=depth,
+        tileScale=16)
+
+    return ee.Feature(ee.Feature(None, s).copyProperties(image).copyProperties(point)).set({
+
+        'PTLON': ee.Number(ee.List(point.geometry().coordinates()).get(0)),
+        'PTLAT': ee.Number(ee.List(point.geometry().coordinates()).get(1)),
+        'OBSDATE': ee.Date(image.get('system:time_start')).format(),
+        'MSTIME': image.get('system:time_start')
+
+    })
+
+
+def sampleGridCore(image, grid, scale):
+
+    depth = image.bandNames().length()
+    samples = grid.map(lambda point: get_point(image, point, scale, depth))
+
+    return samples
+
+
+def sampleGridTS(imagecoll, grid, scale, control, doexport, fname):
+
+    samples = imagecoll.map(
+       lambda image: sampleGridCore(image, grid, scale)
+    ).flatten()
+  
+    samples = samples.filter(ee.Filter.neq(control, None))
+  
+    if doexport:
+        t = ee.batch.Export.table.toDrive(samples, fname, '')
+        t.start()
+
+        return {'samples':samples, 'task':t}
+
+    else:
+        return samples
