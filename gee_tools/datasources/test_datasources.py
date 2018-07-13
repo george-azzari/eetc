9309156166af8c2725e5c80cd68f712a13643286
Anthony Perez <anthonyperez@Anthonys-MacBook-Pro.local>
2018-07-13 14:21:38 -0700

    Added a join DMSP VIIRS datasource

--- gee_tools/datasources/test_datasources.py
+++ gee_tools/datasources/test_datasources.py
@@ -1,7 +1,9 @@
 """nonoptical_datasources unit test"""
-from nonoptical_datasources import DMSPCalV4, VIIRSMonthlyStrCorr, SRTMElevation, Palsar
-import constants
+from nonoptical_datasources import DMSPCalV4, VIIRSMonthlyStrCorr, SRTMElevation, Palsar, DMSPCalVIIRSJoined 
 import ee
+
+class constants:
+    EPSG3857 = 'EPSG:3857'
 
 def test1(start_date, end_date, use_viirs, test_palsar=True):
     scale = 30
@@ -21,8 +23,28 @@ def test1(start_date, end_date, use_viirs, test_palsar=True):
     if test_palsar and len(palsar_bands) != 5:
         raise ValueError("Invalid pulsar bands\n{}".format(palsar_bands))
 
-def test2(start_date, end_date, use_viirs):
-    pass
+def test2():
+    nl = DMSPCalVIIRSJoined('2009-1-1', '2011-12-31').get_img_coll()
+
+    size = nl.size().getInfo()
+    if size != 2:
+        raise ValueError("DMSP VIIRS Joined should have 3 image for the '2009-1-1', '2011-12-31' period.  Had: {}".format(size))
+
+    nl = DMSPCalVIIRSJoined('2012-1-1', '2014-12-31').get_img_coll()
+
+    if nl.size().getInfo() != 12:
+        raise ValueError("DMSP VIIRS Joined should have 1 image for the '2012-1-1', '2014-12-31' period")
+
+    nl = DMSPCalVIIRSJoined('2015-1-1', '2017-12-31').get_img_coll()
+
+    if nl.size().getInfo() != 36:
+        raise ValueError("DMSP VIIRS Joined should have 3 images for the '2015-1-1', '2017-12-31' period")
+
+    nl = DMSPCalVIIRSJoined('2012-1-1', '2013-12-31').get_img_coll()
+    if nl.size().getInfo() != 0:
+        raise ValueError("No imagery should exist for the '2012-1-1', '2013-12-31' period for DMSP VIIRS Joined")
+
+    nl = DMSPCalVIIRSJoined('2015-1-1', '2015-12-31').get_img_coll().median().getInfo()
 
 
 if __name__ == "__main__":
@@ -30,3 +52,4 @@ if __name__ == "__main__":
     test1('2009-1-1', '2011-12-31', False)
     test1('2012-1-1', '2014-12-31', True, test_palsar=False)
     test1('2015-1-1', '2017-12-31', True)
+    test2()
