10319448f6c08a809621da3ae27938d0fcb3d5ce
Anthony Perez <anthonyperez@Anthonys-MacBook-Pro.local>
2018-07-25 19:15:45 -0700

    Renamed name paramater to image_args or coll_args to indicate that it could be any constructor argument

--- gee_tools/datasources/generic_datasources.py
+++ gee_tools/datasources/generic_datasources.py
@@ -7,12 +7,12 @@ import ee
 from gee_tools.datasources.interface import MultiImageDatasource, GlobalImageDatasource, SingleImageDatasource, DatasourceError
 
 class GenericSingleImageDatasource(SingleImageDatasource):
-    """Generic SingleImageDatasource for loading an image by name."""
+    """Generic SingleImageDatasource for loading an image by name or arguments to ee.Image."""
 
-    def build_img_coll(self, name=None):
-        if name is None:
-            raise ValueError('name must be provided, but was None')
-        self.img = ee.Image(name)
+    def build_img_coll(self, image_args=None):
+        if image_args is None:
+            raise ValueError('image_args must be provided, but was None')
+        self.img = ee.Image(image_args)
         self.coll = ee.ImageCollection([self.img])
 
     def get_img_coll(self):
@@ -20,12 +20,12 @@ class GenericSingleImageDatasource(SingleImageDatasource):
 
 
 class GenericGlobalImageDatasource(GlobalImageDatasource):
-    """Generic GlobalImageDatasource for loading an image by name."""
+    """Generic GlobalImageDatasource for loading an image by name or arguments to ee.ImageCollection."""
 
-    def build_img_coll(self, name=None):
-        if name is None:
-            raise ValueError('name must be provided, but was None')
-        self.coll = ee.ImageCollection(name) \
+    def build_img_coll(self, coll_args=None):
+        if coll_args is None:
+            raise ValueError('coll_args must be provided, but was None')
+        self.coll = ee.ImageCollection(coll_args) \
                       .filterDate(self.start_date, self.end_date) \
                       .sort('system:time_start')
 
@@ -34,12 +34,12 @@ class GenericGlobalImageDatasource(GlobalImageDatasource):
 
 
 class GenericMultiImageDatasource(MultiImageDatasource):
-    """Generic GlobalImageDatasource for loading an image by name."""
+    """Generic GlobalImageDatasource for loading an image by name or arguments to ee.ImageCollection."""
 
-    def build_img_coll(self, name=None):
-        if name is None:
-            raise ValueError('name must be provided, but was None')
-        self.coll = ee.ImageCollection(name) \
+    def build_img_coll(self, coll_args=None):
+        if coll_args is None:
+            raise ValueError('coll_args must be provided, but was None')
+        self.coll = ee.ImageCollection(coll_args) \
                       .filterDate(self.start_date, self.end_date) \
                       .filterBounds(self.filterpoly) \
                       .sort('system:time_start')
