e039197818cecca0e7f70d31ef375a89a9f4d60f
Shruti Jain <shruti.j.90@gmail.com>
2020-05-14 14:18:04 -0700

    added export grid core to export_tables and editied glcm function in imgtools

--- gee_tools/export_tables.py
+++ gee_tools/export_tables.py
@@ -34,28 +34,38 @@ def get_point(image, point, scale, depth):
         maxPixels=depth,
         tileScale=16)
 
-    return ee.Feature(ee.Feature(None, s).copyProperties(image).copyProperties(point)).set({
+    return ee.Feature(ee.Feature(None, s).copyProperties(image, None, image.bandNames()).copyProperties(point, None, image.bandNames())).set({
 
-        'PTLON': ee.Number(ee.List(point.geometry().coordinates()).get(0)),
-        'PTLAT': ee.Number(ee.List(point.geometry().coordinates()).get(1)),
-        'OBSDATE': ee.Date(image.get('system:time_start')).format(),
-        'MSTIME': image.get('system:time_start')
+        #'PTLON': ee.Number(ee.List(point.geometry().coordinates()).get(0)),
+        #'PTLAT': ee.Number(ee.List(point.geometry().coordinates()).get(1)),
+        #'OBSDATE': ee.Date(image.get('system:time_start')).format(),
+        #'MSTIME': image.get('system:time_start')
 
     })
 
 
-def reducegrid_image(image, grid, scale):
-
+def reducegrid_core(image, grid, scale):
     depth = image.bandNames().length()
     samples = grid.map(lambda point: get_point(image, point, scale, depth))
-
     return samples
+
+
+def reducegrid_image(image, grid, scale, control, doexport, fname):
+
+    samples = reducegrid_core(image, grid, scale)
+
+    if doexport:
+        t = export_features(samples, fname, export_to='drive')
+        return {'samples':samples, 'task':t}
+
+    else:
+        return samples
 
 
 def reducegrid_imgcoll(imagecoll, grid, scale, control, doexport, fname):
 
     samples = imagecoll.map(
-       lambda image: reducegrid_image(image, grid, scale)
+       lambda image: reducegrid_core(image, grid, scale)
     ).flatten()
   
     samples = samples.filter(ee.Filter.neq(control, None))
