fefedb7ca296192bae02df8ce6c85e84d8b6e05e
George Azzari <george.azzari@gmail.com>
2022-07-12 15:12:11 -0700

    Committing changes made a long time ago - will address later

--- gee_tools/harmonics.py
+++ gee_tools/harmonics.py
@@ -5,12 +5,36 @@ Department of Earth System Science
 Stanford University
 """
 
+# pylint: disable=g-bad-import-order
+# Using lowercase function naming to match the JavaScript names.
+# pylint: disable=g-bad-name
+
 import ee
 import numpy as np
 import gee_tools.datasources.optical_datasources as optix
 
+from __future__ import print_function
 
-def append_band(current, previous):
+
+def addTimeUnit(image, refdate):
+
+    date = image.date()
+  
+    # Letting user pick the reference date
+    dyear = date.difference(refdate, 'year')
+    t = image.select(0).multiply(0).add(dyear).select([0],['t']).float()
+    
+    imageplus = image.addBands(t)
+
+    return imageplus.set('timeunit', dyear)
+
+
+def addConstant(image):
+
+    return image.addBands(image.select(0).multiply(0).add(1).select([0], ['constant']))
+
+
+def appendBand(current, previous):
 
     # Append it to the result (Note: only return current item on first element/iteration)
     accum = ee.Algorithms.If(ee.Algorithms.IsEqual(previous, None), current, current.addBands(ee.Image(previous)))
@@ -18,64 +42,66 @@ def append_band(current, previous):
     return accum
 
 
-def add_timevars(image, tunit):
+def getNthHarmonic(n, omega, timeband):
+    
+    timerad = timeband.multiply(ee.Number(n)).multiply(ee.Number(2*np.pi)).multiply(ee.Number(omega))
+    
+    hcos = timerad.cos().rename(ee.String('cos').cat(ee.String(ee.Number(n).toInt())))
+    hsin = timerad.sin().rename(ee.String('sin').cat(ee.String(ee.Number(n).toInt())))
 
-    # TODO: update this to be a simpler add_timeunit. Other info are irrelevant to the regression.
-
-    date = image.date()
-    doy = date.getRelative('day', 'year').add(1)
-    doyimage = image.select(0).multiply(0).add(doy).select([0], ['DOY']).toInt16()
-
-    month = date.getRelative('month', 'year')
-    monthimage = image.select(0).multiply(0).add(month).select([0], ['MONTH']).toInt16()
-
-    jan1 = ee.Date.fromYMD(date.get('year'), 1, 1)
-    dyear = date.difference(jan1, 'year')
-    dyimage = image.select(0).multiply(0).add(dyear).select([0], ['DYEAR']).float()
-
-    ms = ee.Number(image.get('system:time_start'))
-    msimage = image.select(0).multiply(0).add(ms).select([0], ['MSTIME'])
-
-    imageplus = ee.Image.cat([image, doyimage, monthimage, msimage, dyimage])
-    imageplus = imageplus.set({'DOY': doy, 'MONTH': month, 'YEAR': date.get('year'), 'DYEAR': dyear})
-
-    bnames = ee.List(imageplus.bandNames()).replace(tunit, 't')
-
-    return imageplus.rename(bnames)
+    return ee.List([hcos, hsin])
 
 
-def add_constant(image):
+def addHarmonics(image, timeband, omega, nharmonics):
 
-    return image.addBands(image.select(0).multiply(0).add(1).select([0], ['constant']))
+    timeradians = ee.List.sequence(1, nharmonics, 1).map(lambda n: getNthHarmonic(n, omega, timeband)).flatten()
 
+    # Convert list into a collection and smash into an image.
+    timeradsimg = ee.Image(ee.ImageCollection.fromImages(timeradians).iterate(appendBand))
+  
+    constant = image.select(timeband).divide(image.select(timeband)).rename('constant')
 
-def add_harmonics(image, omega):
-
-    image = add_timevars(image, 'DYEAR')
-    image = add_constant(image)
-
-    timeRadians = image.select('t').multiply(2 * np.pi * omega)
-    timeRadians2 = image.select('t').multiply(4 * np.pi * omega)
-
-    cost = timeRadians.cos().rename(['cos'])
-    sint = timeRadians.sin().rename(['sin'])
-    cost2 = timeRadians2.cos().rename(['cos2'])
-    sint2 = timeRadians2.sin().rename(['sin2'])
-
-    return image.addBands(cost).addBands(sint).addBands(cost2).addBands(sint2)
+    # Updating the input image w/ new harmonic bands
+    image = image.addBands(timeradsimg).addBands(constant)
+    image = image.set('independents', timeradsimg.bandNames().add(timeband).add('constant'))
+  
+    return image
 
 
-def get_harmonic_coll(collection, omega=1.5):
+def getHarmonicCollection(collection, omega, nharmonics, timeband, refdate):
+    """
+    Attach harmonic independents to an input image collection
+    """
 
-    f = lambda img: add_harmonics(img, omega)
+    if nharmonics is None:
+        nharmonics = 2
+    
+    # If there is no time band in the collection generate the standard one
+    if timeband is None:
+        # Without reference date, use Jan-1 of the collection's first year.
+        if refdate is None:
+            d = ee.Image(collection.first()).date()
+            refdate = ee.Date.fromYMD(d.get('year'), 1, 1)
 
-    # Add harmonic terms as new image bands.
-    harmonic_coll = collection.map(f)
+        timeband = 't'
+        collection = collection.map(lambda img: addTimeUnit(img, refdate)) 
 
-    return harmonic_coll
+    # Add harmonic terms as new image bands to collection.
+    harmonic_coll = collection.map(lambda img: addHarmonics(img, timeband, omega, nharmonics))
+
+    return  harmonic_coll
 
 
-def _arrayimg_hrmregr_single(harmonicoll, dependent, independents):
+# def getDummyTime(startdate, enddate, ndays, dummyimg, addharmonics, omega, nharmonics):
+
+#     diff = enddate.difference(startdate, 'day')
+#     increments = ee.List.sequence(1, diff, null, ndays)
+  
+#     # Had to add the "round" part to avoid sub-daily increments when ndays is not 365.
+#     dates = increments.map(lambda i: startdate.advance(ee.Number(i).round(), 'day'))
+
+
+def arrayimgHarmonicRegr(harmonicoll, dependent, independents):
 
     """
      The first output is a coefficients array with dimensions (numX, numY)
@@ -97,13 +123,13 @@ def _arrayimg_hrmregr_single(harmonicoll, dependent, independents):
     return regression
 
 
-def hrmregr_single(harmonicoll, dependent, independents):
+def imageHarmonicRegr(harmonicoll, dependent, independents):
 
     independents = ee.List(independents)
     dependent = ee.String(dependent)
 
-    hregr = _arrayimg_hrmregr_single(harmonicoll, dependent, independents)
-
+    hregr = arrayimgHarmonicRegr(harmonicoll, dependent, independents)
+    
     # New names for coefficients
     newnames = independents.map(lambda b: dependent.cat(ee.String('_')).cat(ee.String(b)))
     # Turn the array image into a multi-band image of coefficients.
@@ -113,10 +139,14 @@ def hrmregr_single(harmonicoll, dependent, independents):
     # The band 'residuals' the *root mean square* of the residuals (RMSE)
     rmse = hregr.select('residuals').arrayProject([0]).arrayFlatten([[dependent.cat(ee.String('_rmse'))]])
 
-    # Computing variance for R2
-    totreducer = ee.Reducer.sampleVariance()
-    variance = harmonicoll.select(dependent).reduce(totreducer)
+    # Computing variance and mean across collection (needed for R2)
+    totreducer = ee.Reducer.sampleVariance().combine(ee.Reducer.count(), None, True)
+    totreducer = totreducer.combine(ee.Reducer.mean(), None, True)
+    stats = harmonicoll.select(dependent).reduce(totreducer)
 
+    varbname = dependent.cat(ee.String('_variance'))
+    variance = stats.select(varbname)
+    
     # UPDATE: computing R2 from RMSE and variance can be done on the fly.
     #         Suppressing it here can save storage if asset is exported.
     # # Computing R2
@@ -126,6 +156,21 @@ def hrmregr_single(harmonicoll, dependent, independents):
     imgcoeffs = imgcoeffs.addBands(rmse).addBands(variance)#.addBands(r2)
 
     return imgcoeffs
+
+
+def getHarmonicCoeffs(harmonicoll, bands, independents, ascoll):
+
+    if independents is None:
+        independents = ee.List(ee.Image(harmonicoll.first()).get('independents'))
+
+    coefficients = ee.List(bands).map(lambda band: imageHarmonicRegr(harmonicoll, band, independents))
+    coeffcoll = ee.ImageCollection.fromImages(coefficients)
+
+    if ascoll:
+        return coeffcoll
+    else:
+        return ee.Image(coeffcoll.iterate(appendBand))
+
 
 
 def _get_prediction(harmonicimg, regrcoeffimg, dependent, independents):
@@ -163,7 +208,7 @@ def hrmregr_multi(harmonicoll, dependents, independents, ascoll):
         return coeffcoll
 
     else:
-        return ee.Image(coeffcoll.iterate(append_band))
+        return ee.Image(coeffcoll.iterate(appendBand))
 
 
 def lx_hregr(region, start_date, end_date, omega=1.5, imgmask=None, bands=None, rmbands=None,
